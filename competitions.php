0076a4e1e09985c3ae8b8690cce7ee8928b4be14
Tiasa Kim <32970989+tiasakim@users.noreply.github.com>
2022-12-19 17:25:48 -0500

    update competition eligibility

---
+++ competitions.php
@@ -0,0 +1,85 @@
+<?php
+    $pageTitle = 'Startup Pitch Competition | MIT Global Startup Workshop 2023';
+    include('templates/nav-bar.php');
+?>
+
+<div class="parallax-window header" data-parallax="scroll" data-image-src="image/">
+    <h1 class="parallax-text">
+        Startup Pitch Competition
+    </h1>
+</div>
+<div class="section competition-section">
+    <div class="row">
+            <div class="row header-row">
+                <div class="col-md-2 competitions-highlights">
+                    <img src="splash/img/bpc-thumbnail.png" class="competition-img" />
+                </div>
+                <div class="col-md-1"></div>
+                <div class="col-md-9 marginTop">
+                    <p class="date">
+                        TBD
+                        <br>
+                        <span class="competition-location">TBD</span>
+                    </p>
+                    <p class="competition-slogan">Are you an entrepreneur trying to take your startup to the next level?<br>
+                        The MIT GSW Business Plan Competition is an opportunity for you to pitch your company and compete for a chance to win seed money to scale your startup.
+                        <br>
+                        <br>
+                        <span class="competition-rules">For additional detail on format and eligibility requirements, <a class="competition-rules-link" href="Rules.pdf"><strong>please read the BPC Rules here</strong></a> before applying.</span>
+                    </p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    <h2 class="competition-subtitle">Format</h2>
+                    <p class="competition-text">Selected finalists for our Business Plan Competition will receive direct feedback from experienced judges and mentors, and have a chance to win prizes to advance their startups.</p>
+                    <p class="competition-text">Finalists automatically receive a spot in the <a href="startup-showcase.php">Startup Showcase</a> as well.</p>
+                    <ul class="competition-text">
+                        <li class="list-competition">Up to <span style="font-weight: 800">12 teams</span> present their current startup to a panel of experienced judges</li>
+                        <li class="list-competition">Receive <span style="font-weight: 800">1-on-1 mentorship</span> from entrepreneurs and business leaders from MIT and Thailand</li>
+                    </ul>
+                </div>
+                <div class="col-md-6">
+                    <h2 class="competition-subtitle">Eligibility Criteria</h2>
+                    <ul class="competition-text">
+                    Teams must meet all the following requirements to be eligible to compete in the Startup Pitch Competition.
+                    
+                        <li class="list-competition">Applicant Eligibility: Anyone from any discipline and from any organization, including colleges and universities, government laboratories, research institutions, non-profit organizations, and for-profit companies</li>
+                        <li class="list-competition">Stage Eligibility: Startups should have a beta product/service/prototype. Ideation startups are not eligible to compete</li>
+                        <li class="list-competition">Prior Funding Limitations: Startups that have raised more than EUR 1,000,000  in equity capital from sources other than self-funding, friends, or family prior to March 2023 are not eligible to compete. Teams can engage in discussions to raise equity before the conference, but shouldn’t have officially raised more than the maximum amount before the conference.</li>
+                    In addition, the following funding sources do not contribute towards the EUR 1,000,000 limit:
+                      <li> Contributions from friends, family, and mentors</li>
+                      <li> Research and funding grants</li>
+                      <li> Winnings from prior competitions</li>
+                      <li>Traditional debt instruments (does not include convertible notes towards equity)</li> 
+
+     
+                  </ul>
+                  
+                  <!--
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <h2 class="competition-subtitle">Prizes</h2>
+                            <ul class="competition-text">
+                                <li class="list-competition">First Place: <strong>$10,000 USD</strong> cash prize</li>
+                                <li class="list-competition">Second Place: <strong>$2,000 USD</strong> cash prize</li>
+                                <li class="list-competition">The winner and runner up will be matched with an MIT student and directly accepted to the final round of the MIT $100K Competition</li>
+                            </ul>
+                        </div>
+                    </div>
+                  -->
+                </div>
+            </div>
+
+            <div class="row marginTop">
+                <div class="col-xs-12 text-center">
+                    <a class="btn btn-default register-button" href="https://mit.co1.qualtrics.com/jfe/form/SV_6rmPilYlw4kVkMZ" role="button">Apply now to BPC</a><br>
+                    <span class="competition-text">Applications due by 11:59 PM ICT on Jan 20th, 2023</span>
+                </div>
+            </div>
+    </div>
+</div>
+
+<?php
+include("templates/footer.php");
+?>
