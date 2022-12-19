aa35e922d332de806b686892301bad97bf6b52b2
Tiasa Kim <32970989+tiasakim@users.noreply.github.com>
2022-12-19 13:59:14 -0500

    Update team.php

--- team.php
+++ team.php
@@ -35,8 +35,7 @@
     	color: white;
 	font-size: 12px;
 	position: absolute;
-	top: 50%;
-	left: 50%;
+	
 	-webkit-transform: translate(-50%, -50%);
 	-ms-transform: translate(-50%, -50%);
 	transform: translate(-50%, -50%);
