fc9070ae06dce0eb0597deeab7e846321c465506
Tiasa Kim <32970989+tiasakim@users.noreply.github.com>
2022-12-19 18:16:23 -0500

    formatting fix

--- competitions.php
+++ competitions.php
@@ -57,13 +57,12 @@
 </div>
 <div class="section">
 	<div style="color:white;">
-        Are you an entrepreneur trying to take your startup to the next level?<br>
-        The MIT GSW Startup Pitch Competition is an opportunity for you to pitch your company and compete for a chance to win seed money to scale your startup.
+        Are you an entrepreneur trying to take your startup to the next level? The MIT GSW Startup Pitch Competition is an opportunity for you to pitch your company and compete for a chance to win seed money to scale your startup.
         <!--<p class="competition-slogan">Are you an entrepreneur trying to take your startup to the next level?<br>
         The MIT GSW Business Plan Competition is an opportunity for you to pitch your company and compete for a chance to win seed money to scale your startup.
         <br><br><span class="competition-rules">For additional detail on format and eligibility requirements, <a class="competition-rules-link" href="Rules.pdf"><strong>please read the BPC Rules here</strong></a> before applying.</span>
         </p>
-	    -->
+	-->
     </div>
 
 	<br>
@@ -71,19 +70,18 @@
 	<div class="section-header section-header-page" style="color: white">Eligibility Criteria</div>
 	<div class="row team-row text-center">
 		<div class="team container col-md-6">
-            <h2 class="competition-subtitle">Eligibility Criteria</h2>
-            <ul class="competition-text"> 
-                Teams must meet all the following requirements to be eligible to compete in the Startup Pitch Competition.
-                <li class="list-competition">Applicant Eligibility: Anyone from any discipline and from any organization, including colleges and universities, government laboratories, research institutions, non-profit organizations, and for-profit companies</li>
-                <li class="list-competition">Stage Eligibility: Startups should have a beta product/service/prototype. Ideation startups are not eligible to compete</li>
-                <li class="list-competition">Prior Funding Limitations: Startups that have raised more than EUR 1,000,000  in equity capital from sources other than self-funding, friends, or family prior to March 2023 are not eligible to compete. Teams can engage in discussions to raise equity before the conference, but shouldn’t have officially raised more than the maximum amount before the conference.</li>
-                In addition, the following funding sources do not contribute towards the EUR 1,000,000 limit:
-                <li> Contributions from friends, family, and mentors</li>
-                <li> Research and funding grants</li>
-                <li> Winnings from prior competitions</li>
-                <li>Traditional debt instruments (does not include convertible notes towards equity)</li> 
-            </ul>
-        </div>   
+		    <ul class="competition-text"> 
+			Teams must meet all the following requirements to be eligible to compete in the Startup Pitch Competition.
+			<li class="list-competition">Applicant Eligibility: Anyone from any discipline and from any organization, including colleges and universities, government laboratories, research institutions, non-profit organizations, and for-profit companies</li>
+			<li class="list-competition">Stage Eligibility: Startups should have a beta product/service/prototype. Ideation startups are not eligible to compete</li>
+			<li class="list-competition">Prior Funding Limitations: Startups that have raised more than EUR 1,000,000  in equity capital from sources other than self-funding, friends, or family prior to March 2023 are not eligible to compete. Teams can engage in discussions to raise equity before the conference, but shouldn’t have officially raised more than the maximum amount before the conference.</li>
+			In addition, the following funding sources do not contribute towards the EUR 1,000,000 limit:
+			<li class="list-competition"> Contributions from friends, family, and mentors</li>
+			<li class="list-competition"> Research and funding grants</li>
+			<li class="list-competition"> Winnings from prior competitions</li>
+			<li class="list-competition">Traditional debt instruments (does not include convertible notes towards equity)</li> 
+		    </ul>
+        	</div>   
 	</div>
 		
 <div class="section">
