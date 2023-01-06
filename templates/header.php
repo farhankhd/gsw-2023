3ab4c4f29006fd066bfc73a18f599902ecc88605
Tiasa Kim <32970989+tiasakim@users.noreply.github.com>
2023-01-06 17:13:35 +0100

    fix style tag syntax

--- templates/header.php
+++ templates/header.php
@@ -61,7 +61,7 @@ function auto_version($file)
             		<i><h3>25th Annual Conference:</h3></i>
 			<i><h2>Athens, Greece</h2></i>
 			<i><h2>March 30-31, 2023</h2></i>
-			<div class='coming-soon' style='display:flex justify-content:center;'>
+			<div class='coming-soon' style='display:flex; justify-content:center'>
 				<div class='container-day'>
 					<h3 class='day'>Time</h3>
 					<h3>Day</h3>
