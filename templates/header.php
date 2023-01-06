1636b7435840f278b410adaee0275da92ac94a6c
Tiasa Kim <32970989+tiasakim@users.noreply.github.com>
2023-01-06 17:11:00 +0100

    Update header.php

--- templates/header.php
+++ templates/header.php
@@ -63,33 +63,7 @@ function auto_version($file)
 			<i><h2>March 30-31, 2023</h2></i>
 		</div>
 		
-		
-		<!--live countdown
-		<div class='coming-soon'>
-			<div class="countdown">
-				<div class='container-day'>
-				    <h3 class='day'>Time</h3>
-				    <h3>Day</h3>
-				</div>
-
-				<div class='container-hour'>
-				    <h3 class='hour'>Time</h3>
-				    <h3>Hour</h3>
-				</div>
-
-				<div class='container-minute'>
-				    <h3 class='minute'>Time</h3>
-				    <h3>Minute</h3>
-				</div>
-
-				<div class='container-second'>
-				    <h3 class='second'>Time</h3>
-				    <h3>Second</h3>
-				</div>
-			</div>
-		 </div>
-		-->
-		<div class='coming-soon'>
+		<div class='coming-soon' style='display:flex justify-content:center;'>
 				<div class='container-day'>
 					<h3 class='day'>Time</h3>
 					<h3>Day</h3>
@@ -110,6 +84,7 @@ function auto_version($file)
 					<h3>Second</h3>
 				</div>
 			</div>
+		
 		<br> <br><br> <br><br> <br>
 	</div>
 	
