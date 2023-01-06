2ad5e51a9def1200c1852974a237ce39234071b9
Tiasa Kim <32970989+tiasakim@users.noreply.github.com>
2023-01-06 17:08:09 +0100

    Update header.php

--- templates/header.php
+++ templates/header.php
@@ -61,7 +61,6 @@ function auto_version($file)
             		<i><h3>25th Annual Conference:</h3></i>
 			<i><h2>Athens, Greece</h2></i>
 			<i><h2>March 30-31, 2023</h2></i>
-			<br><br><br>
 			<div class='coming-soon'>
 				<div class='container-day'>
 					<h3 class='day'>Time</h3>
