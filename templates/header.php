a3cb556515b2e7ce7b48d0320a38ef19198d26be
Tiasa Kim <32970989+tiasakim@users.noreply.github.com>
2023-01-06 16:22:21 +0100

    Update header.php

--- templates/header.php
+++ templates/header.php
@@ -62,7 +62,7 @@ function auto_version($file)
 			<i><h2>Athens, Greece</h2></i>
 			<i><h2>March 30-31, 2023</h2></i>
 			<br><br><br>
-			<i class="countdown">
+			<i>
 				<i class='container-day'>
 				    <h3 class='day'>Time</h3>
 				    <h3>Day</h3>
