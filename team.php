51b36fa37c322b06d95264ddc3fa2c256bd1cb46
Tiasa Kim <32970989+tiasakim@users.noreply.github.com>
2023-01-17 07:48:34 -0500

    add advisory board

--- team.php
+++ team.php
@@ -361,11 +361,51 @@
 			<div class="team-name">Noah Roberts</div>
 			<div class="team-role">Finance Lead</div>
 			<div class="team-area">MIT MBA '24</div>
+		</div>
         	</div>
-        	</div>
-	    
+	<div class="section-header section-header-page" style="color: white">Advisory Board</div>
+	    <div class="row text-center team">
+		<div class="col-md-4">
+		    <img src="images/team/Megan.jpg" class="team-picture" alt="">
+		    <div class="team-name">Megan Mitchell</div>
+		    <div class="team-role">Acting Director</div>
+		    <div class="team-area">MIT Legatum Center</div>
+		</div>
+		<div class="col-md-4">
+		    <img src="images/team/lee.jpg" class="team-picture" alt="">
+		    <div class="team-name">Lee Ullmann</div>
+		    <div class="team-role">Executive Director</div>
+		    <div class="team-area">MIT Sloan Latin America Office</div>
+		</div>
+		<div class="col-md-4">
+		    <img src="images/team/Travis.jpg" class="team-picture" alt="">
+		    <div class="team-name">Travis Hunter</div>
+		    <div class="team-role">Director</div>
+		    <div class="team-area">MIT Regional Entrepreneurship & Acceleration Program</div>
+		</div>
+	    </div>
+	    <div class="row team-row text-center">
+		<div class="team col-md-4">
+		    <img class="team-picture" src="images/team/Diana.jpg">
+		    <div class="team-name">Diana Lu</div>
+		    <div class="team-role">GSW Director 2018</div>
+		    <div class="team-area">MIT Health Sciences Technology</div>
+		</div>
+		<div class="team col-md-4">
+		    <img class="team-picture" src="images/team/David.jpg">
+		    <div class="team-name">David Miranda Nieves</div>
+		    <div class="team-role">GSW Director 2018</div>
+		    <div class="team-area">MIT Health Sciences Technology</div>
+		</div>
+		<div class="team col-md-4">
+		    <img class="team-picture" src="images/team/Juan.jpg">
+		    <div class="team-name">Juan Ruiz Ruiz</div>
+		    <div class="team-role">GSW Director 2018</div>
+		    <div class="team-area">MIT Nuclear Science and Engineering</div>
+		</div>
+	    </div>
+	</div>
 		
-
 <div class="section">
     <hr />
 </div>
