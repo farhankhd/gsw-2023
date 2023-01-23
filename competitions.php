380ba7956bad39d760ebee701d7b6e99f92c7384
Tiasa Kim <32970989+tiasakim@users.noreply.github.com>
2023-01-22 19:53:36 -0500

    Update competitions.php

--- competitions.php
+++ competitions.php
@@ -13,6 +13,21 @@
 
     gtag('config', 'UA-130174054-2');
     gtag('send', 'pageview');
+	    
+	var coll = document.getElementsByClassName("collapsible");
+	var i;
+
+	for (i = 0; i < coll.length; i++) {
+	  coll[i].addEventListener("click", function() {
+	    this.classList.toggle("active");
+	    var content = this.nextElementSibling;
+	    if (content.style.maxHeight){
+	      content.style.maxHeight = null;
+	    } else {
+	      content.style.maxHeight = content.scrollHeight + "px";
+	    } 
+	  });
+	}
     </script>
     
     <style>
