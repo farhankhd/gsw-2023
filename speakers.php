e3197dbe1cc5da76f94b227f6eac49e85cefc8e0
Tiasa Kim <32970989+tiasakim@users.noreply.github.com>
2023-01-31 07:10:37 -0500

    test first batch of rows

--- speakers.php
+++ speakers.php
@@ -14,7 +14,118 @@
 <body style="background-image: url('images/speaker/speaker-background.jpg');">
 
 <div class="section">
-    <div class="row text-center">
+    <div style="color:white;">
+            MIT GSW is proud to host a diverse range of speakers from Cambridge and around the world. 
+            We're excited to have these world class individuals share their experiences with us!
+    	    <!-- <h4 style="text-align: left;"> Click on each speaker to learn more about their background and accomplishments</h4>-->
+    </div>
+	
+	<br>
+	
+	<div class="section-header section-header-page" style="color: white">Keynotes</div>
+	    <div class="row team-row text-center">
+            <div class="team container col-md-6">
+			    <a href="https://www.linkedin.com/in/miles-barr-10401765/">
+			    <img class="team-picture" src="images/speakers/"></a>
+                <div class="overlay">
+                    <div class="team-bio">
+                        Founded Ubiquitous Energy, the world leader in transparent photovoltaics. Our award-winning technology is the world's only truly transparent solar product. 
+                        <br>
+                        <br>
+                        <div id="idLinkedInIcon" onclick="https://www.linkedin.com/in/miles-barr-10401765"</div>
+                    </div>
+
+                </div>
+                <div class="team-name">Miles Barr</div>
+                <div class="team-role">Sustainability</div>
+                <!--<div class="team-area">MIT School of Engineering</div>-->
+            </div>
+            
+            <div class="team container col-md-6">
+			    <a href="">
+			    <img class="team-picture" src="images/speakers/Farhan.jpg"></a>
+                <div class="overlay">
+                    <div class="team-bio">
+                        Andrey Zarur's bio coming soon!
+                        <br>
+                        <br>
+                        <!--
+                        <div id="idLinkedInIcon" onclick="https://www.linkedin.com/in/farhan-khodaee-62212242/');" title="Team | MIT Global Startup Workshop 2023">in</div>
+                        -->
+                    </div>
+
+                </div>
+                <div class="team-name">Andrey Zarur</div>
+                <!--
+                <div class="team-role"></div>
+                <div class="team-area"></div>
+                -->
+            </div>
+    </div>
+    <br>
+    <div class="section-header section-header-page" style="color: white">Panelists</div>
+	    <div class="row team-row text-center">
+            <div class="team container col-md-4">
+			    <a href="https://www.linkedin.com/in/ligris/">
+			    <img class="team-picture" src="images/speakers/kosta-ligris.jpg"></a>
+                <div class="overlay">
+                    <div class="team-bio">
+                        Founder of Ligris, Stavvy
+                        <br>
+                        <br>
+                        <div id="idLinkedInIcon" onclick="https://www.linkedin.com/in/ligris/"</div>
+                    </div>
+
+                </div>
+                <div class="team-name">Kosta Ligris</div>
+                <div class="team-role"></div>
+                <!--<div class="team-area">MIT School of Engineering</div>-->
+            </div>
+            
+            <div class="team container col-md-4">
+			    <a href="https://www.linkedin.com/in/marinahatsopoulos/">
+			    <img class="team-picture" src="images/speakers/marina-hatsopoulos.jpg"></a>
+                <div class="overlay">
+                    <div class="team-bio">
+                        CEO and an original investor in Z Corporation
+                        <br>
+                        <br>
+                        <div id="idLinkedInIcon" onclick="https://www.linkedin.com/in/marinahatsopoulos/"</div>
+                    </div>
+                </div>
+                <div class="team-name">Marina Hatsopoulos</div>
+                <!--
+                <div class="team-role"></div>
+                <div class="team-area"></div>
+                -->
+            </div>
+            <div class="team container col-md-4">
+			    <a href="https://www.linkedin.com/search/results/all/?keywords=WILLIAM%20SONNEBORN&origin=GLOBAL_SEARCH_HEADER&sid=NFR">
+			    <img class="team-picture" src="images/speakers/william-sonneborn.jpg"></a>
+                <div class="overlay">
+                    <div class="team-bio">
+                        Global Director of Disruptive Technologies, PE and VC at IFC
+                        <br>
+                        <br>
+                        <div id="idLinkedInIcon" onclick="https://www.linkedin.com/search/results/all/?keywords=WILLIAM%20SONNEBORN&origin=GLOBAL_SEARCH_HEADER&sid=NFR"</div>
+                    </div>
+                </div>
+                <div class="team-name">William Sonneborn</div>
+                <div class="team-role">Finance</div>
+                <!--
+                <div class="team-area"></div>
+                -->
+            </div>
+    </div>
+</div>
+    
+    
+    
+    
+    
+    
+        <!--
+        <div class="row text-center">
 
         <div class="col-lg-12 comp-content" id="speaker-section-expanded">
             <p class="section-header section-header-page" style="color: white; font-size: 1.7em">Speakers</p>
@@ -24,6 +135,7 @@
 
             <h4 style="text-align: left;"> Click on each speaker to learn more about their background and accomplishments</h4>
             <br> <br>
+        -->
             <!-- <div class="row speaker-expanded-bio">
                 <div class="col-md-3"></div>
                 <div class="col-md-9"></div>
@@ -38,7 +150,11 @@
                 </div>
             </div>
             --->
+            
+            
+     
 
+            <!--
             <div class="col-lg-12 comp-content" id="keynote-section-expanded">
                 <p class="section-header section-header-page" style="color: white">Keynotes</p>
                 <div class="row speaker-expanded-bio">
@@ -47,16 +163,15 @@
                 </div>
             </div>
             <div class="col-lg-12 comp-content" id="speaker-section-expanded">
-                <p class="section-header section-header-page" style="color: white">Speakers</p>
+                <p class="section-header section-header-page" style="color: white">Panelists</p>
                 <div class="row speaker-expanded-bio">
                     <div class="col-md-3"></div>
                     <div class="col-md-9"></div>
                 </div>
-            </div> 
+            </div>
+            -->
+ 
 
-        </div> 
-    </div>
-</div>
 
 <div id="speaker-modals">
 
