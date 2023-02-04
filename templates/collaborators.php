d86a646f553a624294911630c87edf65c1856241
Tiasa Kim <32970989+tiasakim@users.noreply.github.com>
2023-02-04 18:21:17 -0500

    Add files via upload

---
+++ templates/collaborators.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+        <title>MIT GSW 2023</title>
+
+        <!--CSS: Adobe Font, Boostrap, style.css-->
+        <link rel="stylesheet" href="https://use.typekit.net/fww3ner.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel='stylesheet' href='../assets/css/style.css'>
+    </head>
+    <body>
+        <!--ACADEMIC PARTNERS-->
+          
+        <div class="section-div">
+            <img class="banner" src="../img/banner/collaborators.png"> 
+        </div>
+        <section>
+            <div class="row white-bg">
+                <div class="col white-bg">
+                    <h1 class="gradient-text">We're proud to collaborate with these organizations!</h1>
+                </div>
+            </div>
+            <div class="row white-bg logo">
+                <div class="col-sm-6 col-md-3 col-lg white-bg">
+                    <a href="https://entrepreneurship.mit.edu/"><img id="collab-logo" src="../img/collaborators/mtc.png"></a>
+                </div>
+                <div class="col-sm-6 col-md-3 col-lg white-bg">
+                   <a href="https://legatum.mit.edu/"><img id="collab-logo" src="../img/collaborators/legatum.png"></a>
+                </div>
+                <div class="col-sm-6 col-md-3 col-lg white-bg">
+                    <a href="https://reap.mit.edu/"><img id="collab-logo" src="../img/collaborators/reap.png"></a>
+                </div>
+            </div>
+        </section>
+    </body>
+</html
---
+++ templates/sponsors.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+        <title>MIT GSW 2023</title>
+
+        <!--CSS: Adobe Font, Boostrap, style.css-->
+        <link rel="stylesheet" href="https://use.typekit.net/fww3ner.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel='stylesheet' href='../assets/css/style.css'>
+    </head>
+    <body>
+        <!--SPONSORS-->
+        <div class="section-div">
+            <img class="banner" src="../img/banner/sponsors.png"> 
+        </div>
+        <section>
+            <div class="row white-bg">
+                <div class="col white-bg">
+                    <h1 class="gradient-text">Thank you to our Sponsors and Partners! </h1>
+                </div>
+            </div>
+            <div class="row white-bg logo d-flex justify-content-center">
+                <div class="col-sm-6 col-md-3 col-lg-3 white-bg">
+                    <a href=""> <img id="sponsor-logo" src="../img/sponsors/capitalpartner-diamond.png"></a>
+                </div>
+            </div>
+            <div class="row white-bg logo d-flex justify-content-center">
+                <div class="col-sm-6 col-md-3 col-lg-3 white-bg">
+                    <a href="https://shopflix.gr/"><img id="sponsor-logo" src="../img/sponsors/shopflix.png"></a>
+                </div>
+            </div>
+            <div class="row white-bg logo d-flex justify-content-center">
+                <div class="col-sm-6 col-md-3 col-lg-3 white-bg">
+                    <a href="https://www.libra.com/en/"><img id="sponsor-logo" src="../img/sponsors/libragroup.png"></a>
+                </div>
+                <div class="col-sm-6 col-md-3 col-lg-3 white-bg">
+                    <a href=""><img id="sponsor-logo" src="../img/sponsors/scienceagora.png"></a>
+                  
+                </div>
+            </div>
+        </section>
+    </body>
+</html
