fc563a99fa7413605335cb20b1eadd251b6d7543
Tiasa Kim <32970989+tiasakim@users.noreply.github.com>
2023-02-04 17:14:12 -0500

    upload new files

--- index.php
+++ index.php
@@ -1,27 +1,31 @@
 <?php
-
     // Require https
     if ($_SERVER['HTTPS'] != "on") {
         $url = "https://". $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
         header("Location: $url");
         exit;
+        
     }
-
-    $pageTitle = 'MIT Global Startup Workshop 2023';
-    $metaDescription = 'MIT Global Startup Workshop is an annual conference that aims to accelerate entrepreneurial ecosystems around the world, through workshops, mentorship and skills training.'; 
-    $specificKeywords = 'entrepreneurial experience';
-    $headerMainPageImageSrc = 'images/landing-page/bogota.png';
-    $headerTitle = 'MIT Global Startup Workshop';
-    $headerSubTitle = 'GSW 2023';
+    $pageTitle = 'MIT Global Startup Workshop 2023 | Athens, Greece';
+    $metaDescription = 'MIT Global Startup Workshop is coming to Greece in March 2023! GSW is an annual conference that aims to accelerate entrepreneurial ecosystems around the world, through workshops, mentorship and skills training. Grab your tickets now for an experience you will never forget!';
+    $specificKeywords = 'entrepreneurial ecosystems';
+    $headerMainPageImageSrc = 'img/banner/main.png';
+    $headerTitle = 'MIT Global Startup Workshop 2023';
+    $headerSubTitle = 'Athens, Greece';
     $classIndexMenu = 'active';
-
+    
     //set headers to NOT cache a page
     header("Cache-Control: no-cache, must-revalidate"); //HTTP 1.1
     header("Pragma: no-cache"); //HTTP 1.0
     header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
-    
-    include('templates/nav-bar.php');
-    include('templates/header.php');
-    include('templates/footer.php');
+
+    include('php/navbar.php');
+    include('php/home.php');
+    include("php/schedule.php");
 ?>
-<!-- <br /><br />
+<br /><br />
+<?php
+    //include('php/location.php');
+    include('php/collaborators.php');
+    include('php/footer.php');
+?>
