c6be658eb09424c0a6cfc6645f7f803b4b81b568
Tiasa Kim <32970989+tiasakim@users.noreply.github.com>
2023-02-04 19:15:29 -0500

    Update speakers.php

--- templates/speakers.php
+++ templates/speakers.php
@@ -19,12 +19,12 @@ include('markup.php');
         <!--CSS: Adobe Font, Boostrap, style.css-->
         <link rel="stylesheet" href="https://use.typekit.net/fww3ner.css">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <link rel='stylesheet' href='../assets/css/style.css'>
+        <link rel='stylesheet' href='assets/css/style.css'>
     </head>
     <body>
 
         <div class="banner" data-parallax="scroll" data-image-src="">
-            <h1 class="parallax-text">
+            <h1 class="banner-text">
                 Our Speakers
             </h1>
         </div>
