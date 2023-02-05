b1c8ac86a9a5f2943d3c285b52896dfe5c93cf12
Tiasa Kim <32970989+tiasakim@users.noreply.github.com>
2023-02-04 23:34:09 -0500

    Update footer.php

--- templates/footer.php
+++ templates/footer.php
@@ -1,10 +1,18 @@
-<!--include CSS Style Sheet-->
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+        
+         <!--include CSS Style Sheet-->
         <?php echo "<link rel='stylesheet' type='text/css' href='assets/css/style.css' />";?>
 
-<!--include a javascript file-->
+        <!--include a javascript file-->
         <?php echo "<script type='text/javascript' src='assets/js/scripts.js'></script>";?>
+    </head>
 
-<footer>
     <div class="container">
         <div class="row text-center">
             <div class="col-md-offset-2 col-md-8">
@@ -59,7 +67,7 @@
             </div>
         </div>
     </div>
-</footer>
+
 
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
@@ -73,3 +81,5 @@
     });
 </script>
 <?= $scripts ?>
+        
+</html>
