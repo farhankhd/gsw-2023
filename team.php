39c7d70b2ae54e38306b90c2f65313b3aba24063
Tiasa Kim <32970989+tiasakim@users.noreply.github.com>
2023-02-21 10:38:41 -0500

    Update team.php

--- team.php
+++ team.php
@@ -56,7 +56,7 @@
         
         <section class="startup-pitch">
             <div class="row" style="
-                background-image: url(img/team/team_01-sm.jpg);
+                background-image: url(img/team/team_01-v2.jpeg);
                 background-repeat: no-repeat;
                 background-size: cover;height: 344px;">
                 <div class="col">
