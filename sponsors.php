330369ac95cc864ce9b3081cad9431ca2ce05093
Tiasa Kim <32970989+tiasakim@users.noreply.github.com>
2023-03-02 13:58:18 -0500

    Update sponsors.php

--- sponsors.php
+++ sponsors.php
@@ -211,7 +211,7 @@
                 <a href="https://www.moh.gr/en/"> <img style="max-width: 70%; height: auto;" src="/2023/img/sponsors/motoroil.jpeg"></a>
               </div>
               <div class="col-sm-1 col-md-6">
-                <a href="https://www.kotsovolos.gr/"> <img style="max-width: 70%; height: auto;" src="/2023/img/sponsors/dixons.png"></a>
+                <a href="https://www.kotsovolos.gr/"> <img style="max-width: 30%; height: 50%;"" src="/2023/img/sponsors/dixons.png"></a>
               </div>
             </div>
           </div>
@@ -287,10 +287,9 @@
           <hr class="team-hr">
         </div>
         <br>
-        
-         <div class="row white-bg logo d-flex justify-content-center">
-          <div class="col-sm-1 col-md-4">
-            <a href=https://www.thepeoplestrust.org/"><img style="width: 150%; height: auto;" src="/2023/img/sponsors/tpt.png"></a>
+         <div class="row justify-content-center align-items-center">
+          <div class="col-sm-1">
+            <a href=https://www.thepeoplestrust.org/"><img style="width: 120%; height: auto;" src="/2023/img/sponsors/tpt.png"></a>
           </div>
         </div>
       </section>
