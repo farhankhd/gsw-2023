be85aefcfeca04adde2c599af5d54c2b30f1e3d1
Tiasa Kim <32970989+tiasakim@users.noreply.github.com>
2023-03-02 13:03:10 -0500

    Update sponsors.php

--- sponsors.php
+++ sponsors.php
@@ -186,7 +186,7 @@
             <img src="/2023/svg/diamond.svg"/>
           </div>
           <div class="col-sm-1 col-md-12 col-lg-9">
-            <a href="https://www.capitalship.gr/"><img src="/2023/img/sponsors/capital.png"></a>
+            <a href="https://www.capitalship.gr/"><img style="max-width: 80%; height: auto;"src="/2023/img/sponsors/capital.png"></a>
           </div>
         </div>
         
@@ -201,18 +201,18 @@
           <div class="col-sm-1 col-md-12 col-lg-9">
             <div class="row justify-content-center align-items-center">
               <div class="col-sm-1 col-md-6">
-                <a href="https://shopflix.gr/"> <img id="silver-logo" src="/2023/img/sponsors/shopflix.png"></a>
+                <a href="https://shopflix.gr/"> <img style="max-width: 60%; height: auto;"src="/2023/img/sponsors/shopflix.png"></a>
               </div>
               <div class="col-sm-1 col-md-6">
-                <a href="https://www.thesignalgroup.com/"> <img id="silver-logo" src="/2023/img/sponsors/signal.png"></a>
+                <a href="https://www.thesignalgroup.com/"> <img style="max-width: 60%; height: auto;"src="/2023/img/sponsors/signal.png"></a>
               </div>
             </div>
             <div class="row justify-content-center align-items-center">
               <div class="col-sm-1 col-md-6">
-                <a href="https://www.moh.gr/en/"> <img id="silver-logo" src="/2023/img/sponsors/motoroil.jpeg"></a>
+                <a href="https://www.moh.gr/en/"> <img style="max-width: 60%; height: auto;" src="/2023/img/sponsors/motoroil.jpeg"></a>
               </div>
               <div class="col-sm-1 col-md-6">
-                <a href="https://www.kotsovolos.gr/"> <img id="silver-logo" src="/2023/img/sponsors/dixons.png"></a>
+                <a href="https://www.kotsovolos.gr/"> <img style="max-width: 60%; height: auto;" src="/2023/img/sponsors/dixons.png"></a>
               </div>
             </div>
           </div>
@@ -232,7 +232,7 @@
                 <a href="https://www.libra.com/en/"><img style="max-width: 50%; height: auto;" src="/2023/img/sponsors/libragroup.png"></a>
               </div>
               <div class="col-sm-1 col-md-6">
-                <a href="https://www.octaneconsultants.com/"> <img style="max-width: 50%; height: auto;"src="/2023/img/sponsors/octane.jpeg"></a>
+                <a href="https://www.octaneconsultants.com/"> <img style="max-width: 50%; height: auto;" src="/2023/img/sponsors/octane.jpeg"></a>
               </div>
             </div>
             <div class="row justify-content-center align-items-center">
