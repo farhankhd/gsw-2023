cdbb4066cb12e7109ed06f19d63652cbcf3b9c56
Tiasa Kim <32970989+tiasakim@users.noreply.github.com>
2023-03-02 13:39:59 -0500

    Update sponsors.php

--- sponsors.php
+++ sponsors.php
@@ -196,10 +196,10 @@
      
       
         <div class="row align-items-center">
-          <div class="col-sm-1 col-lg-3">
+          <div class="col-sm-1 col-md-12 col-lg-3">
             <img src="/2023/svg/silver.svg"/>
           </div>
-          <div class="col-sm-1 col-lg-9">
+          <div class="col-sm-1 col-md-12 col-lg-9">
             <div class="row justify-content-center align-items-center">
               <div class="col-sm-1 col-md-6">
                 <a href="https://shopflix.gr/"> <img style="max-width: 70%; height: auto;"src="/2023/img/sponsors/shopflix.png"></a>
