e821d0667984fa3bc95bcfee63556ec24ba0af30
Tiasa Kim <32970989+tiasakim@users.noreply.github.com>
2023-03-02 12:02:01 -0500

    Update sponsors.php

--- sponsors.php
+++ sponsors.php
@@ -311,21 +311,21 @@
         
         <div class="row white-bg logo d-flex justify-content-center">
           <div class="col-sm-1 col-md-4">
-            <a href="https://www.mindev.gov.gr/?lang=en"><img id="auspices-logo" src="/2023/img/sponsors/MinDev.png"></a>
+            <a href="https://www.mindev.gov.gr/?lang=en"><img style="max-width: 20%; height: auto;" src="/2023/img/sponsors/MinDev.png"></a>
           </div>
           <div class="col-sm-1 col-md-4">
-            <a href="https://www.minedu.gov.gr/"><img id="auspices-logo" src="/2023/img/sponsors/MinEdu.png"></a>
+            <a href="https://www.minedu.gov.gr/"><img style="max-width: 20%; height: auto;"src="/2023/img/sponsors/MinEdu.png"></a>
           </div>
           <div class="col-sm-1 col-md-4">
-            <a href="https://mindigital.gr/"><img id="auspices-logo" src="/2023/img/sponsors/MinDigGov.png"></a>
+            <a href="https://mindigital.gr/"><img style="max-width: 20%; height: auto;" src="/2023/img/sponsors/MinDigGov.png"></a>
           </div>
         </div>
         <div class="row white-bg logo d-flex justify-content-center">
           <div class="col-sm-1 col-md-4">
-            <a href="https://ypergasias.gov.gr/"><img id="auspices-logo" src="/2023/img/sponsors/MinLSA.png"></a>
+            <a href="https://ypergasias.gov.gr/"><img style="max-width: 20%; height: auto;"src="/2023/img/sponsors/MinLSA.png"></a>
           </div>
           <div class="col-sm-1 col-md-4">
-            <a href="https://www.patt.gov.gr/"><img id="auspices-logo" src="/2023/img/sponsors/attica.png"></a>
+            <a href="https://www.patt.gov.gr/"><img style="max-width:20%; height: auto;"src="/2023/img/sponsors/attica.png"></a>
           </div>
         </div>
       </section>
