57b750fc083e61e2fcb8e99576b7d3ab89368ea5
Tiasa Kim <32970989+tiasakim@users.noreply.github.com>
2023-03-02 17:21:56 -0500

    Update index.php

--- index.php
+++ index.php
@@ -373,7 +373,7 @@
                 <a href="https://entrepreneurship.mit.edu/"><img style="max-width: 90%; height: auto;" src="img/collaborators/mtc.png"></a>
               </div>
               <div class="col-sm-1 col-md-3">
-                <a href="https://legatum.mit.edu/"><img style="max-width: 50%; height: 50%;" src="img/collaborators/legatum.png"></a>
+                <a href="https://legatum.mit.edu/"><img style="max-width: 45%; height: 45%;" src="img/collaborators/legatum.png"></a>
               </div>
               <div class="col-sm-1 col-md-3">
                 <a href="https://reap.mit.edu/"><img style="max-width: 90%; height: auto;" src="img/collaborators/reap.png"></a>
