a0bd857e9dc9832eb0fe149994b30ee9ac8d188c
Tiasa Kim <32970989+tiasakim@users.noreply.github.com>
2023-03-13 00:13:40 -0400

    Update events.php

--- events.php
+++ events.php
@@ -109,19 +109,19 @@
     <div class="row justify-content-md-center">
  
             <!--Moderator: Macaulay Kenney, MIT-->
-            <div class="team container col-md-3"><a href="speakers.php">
+            <div class="col col-lg-3"><a href="speakers.php">
                 <img class="panel-picture" src="img/speakers/MarinaHatsopoulos.jpg"></a>
                 <div class="name">Marina Hatsopoulos</div>
                 <div class="title">Entrepreneur & Writer</div>
                 <div class="title">Board Chair, Levitronix Technologies</div>
             </div>
-            <div class="team container col-md-3"><a href="speakers.php">
+            <div class="col col-lg-3"><a href="speakers.php">
                 <img class="panel-picture" src="img/speakers/LarsRasmussen.jpg"></a>
                 <div class="name">Lars Rasmussen</div>
                 <div class="title">Angel Investor</div>
                 <div class="title">Co-founder of Google Maps</div>
             </div>
-            <div class="team container col-md-3"><a href="speakers.php">
+            <div class="col col-lg-3"><a href="speakers.php">
                 <img class="panel-picture" src="img/speakers/MariaZubeldia.jpg"></a>
                 <div class="name">Maria Zubeldia Diaz</div>
                 <div class="title">Director of Entrepreneurship Centre, Said Business School, University of Oxford</div>
