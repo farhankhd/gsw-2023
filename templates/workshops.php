7daaa5a022a18bd47f8e9e4e6b9cd2775625e49b
Tiasa Kim <32970989+tiasakim@users.noreply.github.com>
2023-03-13 18:33:11 -0400

    Create workshops.php

---
+++ templates/workshops.php
@@ -0,0 +1,227 @@
+<?php
+    $pageTitle = 'MIT Global Startup Workshop 2023 | Athens, Greece';
+    $metaDescription = 'MIT Global Startup Workshop is coming to Greece in March 2023! GSW is an annual conference that aims to accelerate entrepreneurial ecosystems around the world, through workshops, mentorship and skills training. Grab your tickets now for an experience you will never forget!';
+    $specificKeywords = 'speakers, keynotes, Bill Aullet, Miles Barr, Ubiquitous Energy, Andrey Zarur, Kosta Ligris, Marina Hatsopolous, Willliam Sonneborn, MIT';
+
+    include('navbar.php');
+    include('header.php'); 
+?>
+
+<style>
+    .bold-text {
+        font-family: myriad-pro, sans-serif;
+        font-weight: 700;
+        font-style: normal;
+    }
+    .light-text{
+        font-family: myriad-pro, sans-serif;
+        font-style: normal;
+        font-weight: 300px;
+    }
+</style>
+
+<!--PAGE BANNER-->
+<section class="startup-pitch">
+    <div class="row" style="
+        background-image: url(img/banner/banner-02.png);
+        background-repeat: no-repeat;
+        background-size: cover;height: 344px;">
+        <div class="col">
+            <h1 class="section-header competition-text">WORKSHOP OVERVIEW</h1>
+        </div>
+    </div>
+</section>
+
+<br><br><br>
+
+<section class="content-schedule container">
+    <article class="content ">
+        <div class="col-xs-12 col-md-12">
+            <span class="anchor" id="schedule-navigation"></span>
+            <p class="text-center">
+                Subject to change.
+            </p>
+        </div>
+        <br>
+  </article>
+</section>
+
+<!--
+<div class="row">
+    <div class="col">
+            <h1 class="para text-center gradient-text" id="team-section">Workshops</h1>
+            <hr class="team-hr">
+    </div>
+</div>
+<br>
+-->
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col">
+            <img style="max-width: 80%; height: auto;" src="/2023/svg/events/disciplinedentrepreneurship.svg"/>
+        </div>
+    </div>
+    <br>
+    <div class="row justify-content-center">
+        <div class="col-md-9">
+            <p class="bold-text" style="color: #131d25">March 30, 12:30-13:30 (Session 1) // 13:30-14:30 (Session 2) in Athens Concert Hall (Exact Location: TBD)</p>
+            <p class="light-text" style="color: #131d25">
+              Entrepreneurship can be taught. It is not instinctive but rather a systematic problem solving process. 
+              In this workshop, you will get an introduction to the way entrepreneurship is taught in the introductory courses at MIT.
+            </p>
+        </div>
+    </div>
+</div>
+<br>
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col col-lg-2"><a href="speakers.php">
+            <img class="panel-picture" src="img/speakers/BillAulet.jpg"></a>
+            <div class="name">Bill Aulet</div>
+            <div class="title">Professor of the Practice</div>
+            <div class="title">MIT Sloan School of Management</div>
+        </div>
+  </div>
+</div>
+<br><br>
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col">
+            <img style="max-width: 80%; height: auto;" src="/2023/svg/events/scalingup.svg"/>
+        </div>
+    </div>
+    <br>
+    <div class="row justify-content-center">
+        <div class="col-md-9">
+            <p class="bold-text" style="color: #131d25">March 30, 12:30-13:30 (Session 1) // 13:30-14:30 (Session 2) in Athens Concert Hall (Exact Location: TBD)</p>
+            <p class="light-text" style="color: #131d25">
+              So you have started your business. How do you grow it? This workshop will teach you the scaling essentials of building a team, early leadership calibration, capital efficiency, values and tools for a sustainable culture, and how to avoid common pitfalls when scaling quickly. 
+              <br><br>
+              Kosta Ligris is a senior Entrepreneur in Residence at the Martin Trust Center for MIT Entrepreneurship, and Lecturer at MIT Sloan, where he teaches Building an Entrepreneurial Venture. He is the founder and CEO of one of Boston's fastest growing Fintech startups, Stavvy. 
+              Under his leadership, Stavvy (a) raised the largest Series A round in New England history for any PropTech, LegalTech, or FinTech startup and (b) has grown from 8 employees to over 160 employees in 18 months. 
+            </p>
+        </div>
+    </div>
+</div>
+<br>
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col col-lg-2"><a href="speakers.php">
+            <img class="panel-picture" src="img/speakers/KostaLigris.jpg"></a>
+            <div class="name">Kosta Ligris</div>
+            <div class="title">Entrepreneur, EIR, & Lecturer</div>
+        </div>
+    </div>
+</div>
+<br><br>
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col">
+            <img style="max-width: 80%; height: auto;" src="/2023/svg/events/perfectpitch.svg"/>
+        </div>
+    </div>
+    <br>
+    <div class="row justify-content-center">
+        <div class="col-md-9">
+            <p class="bold-text" style="color: #131d25">March 31, 12:30-13:30 (Session 1) // 13:30-14:30 (Session 2) in Athens Concert Hall (Exact Location: TBD)</p>
+            <p class="light-text" style="color: #131d25">
+              When you start a company, you quickly learn that life's a pitch! Which is fine: you want to tell everyone about your amazing innovation, right? 
+              And there is no shortage of people with advice about exactly what and how you should pitch. 
+              The process rapidly gets confusing, stressful, and frustrating.
+              <br><br>
+              Join us for a one hour session on the "Perfect Pitch" method, a simple, straightforward system that helps you to tell your story in your way, while making it clear, concise and compelling to your target audience. 
+              You will learn the 3 parts of every pitch, the 4 types of pitches, and the 10 Questions every pitch should answer. 
+              Each of these tools are designed to produce pitches that are rich in content and value, reflecting your passion, vision and integrity. 
+            </p>
+        </div>
+    </div>
+</div>
+<br>
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col col-lg-2"><a href="speakers.php">
+            <img class="panel-picture" src="img/speakers/LindaPlano.jpeg"></a>
+            <div class="name">Linda Plano</div>
+            <div class="title">Principal Investor, Plano and Simple</div>
+        </div>
+    </div>
+</div>
+<br><br>
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col">
+            <img style="max-width: 80%; height: auto;" src="/2023/svg/events/productdev.svg"/>
+        </div>
+    </div>
+    <br>
+    <div class="row justify-content-center">
+        <div class="col-md-9">
+            <p class="bold-text" style="color: #131d25">March 31, 12:30-13:30 (Session 1) // 13:30-14:30 (Session 2) in Athens Concert Hall (Exact Location: TBD)</p>
+            <p class="light-text" style="color: #131d25">
+              Details coming soon!
+            </p>
+        </div>
+    </div>
+</div>
+<br>
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col col-lg-2"><a href="speakers.php">
+            <img class="panel-picture" src="img/speakers/MacauleyKenney.jpg"></a>
+            <div class="name">Macauley Kenney</div>
+            <div class="title">MIT</div>    
+        </div>
+    </div>
+</div>
+<br><br>
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col">
+            <img style="max-width: 80%; height: auto;" src="/2023/svg/events/openai.svg"/>
+        </div>
+    </div>
+    <br>
+    <div class="row justify-content-center">
+        <div class="col-md-9">
+            <p class="bold-text" style="color: #131d25">Date/Time/Location: TBD</p>
+            <p class="light-text" style="color: #131d25">
+              Introducing OpenAI on Azure, a ChatGPT Talk. Chat GPT, the powerful language model developed by OpenAI, has been generating a lot of excitement in the media and we have received multiple requests to learn more about its capabilities. We are excited to have this opportunity to have a conversation with Startups about the potential use cases for Chat GPT and the Azure OpenAI service. 
+              <br>
+              
+              <ul style="align-content: left; text-align: left;">
+                In this Workshop, we will be discussing
+                <li style="align-content: left; text-align: left;">A demonstration of Chat GPT in action</li>
+                <li style="align-content: left; text-align: left;">A demonstration on Azure OpenAI Services and how to infuse them in your apps.</li>
+              </ul>
+            </p>
+        </div>
+    </div>
+</div>
+<br>
+<!--
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col col-lg-2"><a href="speakers.php">
+            <img class="panel-picture" src="img/speakers/TheodorosEvgeniou.jpg"></a>
+            <div class="name">Theodoros Evgeniou</div>
+            <div class="title">INSEAD Professor</div>
+            <div class="title">Co-founder Tremau</div>
+        </div>
+      </div>
+</div>
+<br><br>
+-->
+<script src="../assets/js/scripts.js"></script>
+
+<?php
+include("templates/footer.php");
+?>
