4ca5880daaca7b5292d1e9d4ad583f7faa172623
Tiasa Kim <32970989+tiasakim@users.noreply.github.com>
2023-03-14 23:47:11 -0400

    Update workshops.php

--- workshops.php
+++ workshops.php
@@ -157,7 +157,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col">
-            <img style="max-width: 80%; height: auto;" src="/2023/img/worrkshop/productdesign.png"/>
+            <img style="max-width: 80%; height: auto;" src="/2023/img/workshop/productdesign.png"/>
         </div>
     </div>
     <br>
