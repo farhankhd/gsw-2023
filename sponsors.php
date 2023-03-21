e766d228e9d3c92b8a138fd1745de9bfe62164e2
Tiasa Kim <32970989+tiasakim@users.noreply.github.com>
2023-03-21 07:18:45 -0400

    Update sponsors.php

--- sponsors.php
+++ sponsors.php
@@ -259,13 +259,13 @@
             </div>
             <div class="row justify-content-center align-items-center">
               <div class="col-sm-1 col-md-3">
-                <a href="https://www.ey.com/en_gr"> <img style="max-width: 30%; height: auto;" src="/2023/img/sponsors/ey.png"></a>
+                <a href="https://www.ey.com/en_gr"> <img style="max-width: 80%; height: auto;" src="/2023/img/sponsors/ey.png"></a>
               </div>
               <div class="col-sm-1 col-md-3">
-                <a href="https://www.hillintl.com/"> <img style="max-width: 40%; height: auto;" src="/2023/img/sponsors/HIL.png"></a>
+                <a href="https://www.hillintl.com/"> <img style="max-width: 80%; height: auto;" src="/2023/img/sponsors/HIL.png"></a>
               </div>
               <div class="col-sm-1 col-md-3">
-                <a href="https://www.kotsovolos.gr/"> <img style="max-width: 40%; height: 30%;" src="/2023/img/sponsors/dixons.png"></a>
+                <a href="https://www.kotsovolos.gr/"> <img style="max-width: 80%; height: 30%;" src="/2023/img/sponsors/dixons.png"></a>
               </div>
             </div>
           </div>
