4ea1f84d7759373700fbd5354fa0a605e15697be
farhankhd <98169192+farhankhd@users.noreply.github.com>
2023-03-25 15:48:02 -0400

    Update sponsors.php

--- sponsors.php
+++ sponsors.php
@@ -389,6 +389,7 @@
         </div>
       </section>
       
+      <!--IDIVIDUAL DONORS-->
       <section>
         <div class="row">
           <div class="col">
@@ -401,7 +402,7 @@
         <br>
          <div class="row justify-content-center align-items-center">
            <div class="speaker col-sm-6 col-md-4 col-lg-3">
-             <a href="https://www.www.miltos.com">
+             <a href="https://www.miltos.com">
              <img class="speaker-picture" src="img/sponsors/MiltosKambourides.jpeg">
                <p class="light-text light-bg-dark-text">Miltos Kambourides </p>
              </a>
