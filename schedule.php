673e80246ae642747c3e362cedba18d1c9309b2c
Tiasa Kim <32970989+tiasakim@users.noreply.github.com>
2023-03-28 05:52:28 -0400

    Update schedule.php

--- schedule.php
+++ schedule.php
@@ -161,7 +161,7 @@
                                         <?php echo "$eventTitle";?>
                                     </h3>
                                     
-                                    <h3 class="justify-content: start" style="font-family: myriad-pro, sans-serif; font-style: normal;font-weight: 150px; color: #00c0f2">
+                                    <h3 class="event_heading" style="font-family: myriad-pro, sans-serif; font-style: normal; font-weight: 150px; color: #00c0f2">
                                         <?php echo "Speaker(s):<br>";
                                             foreach ($eventSpeakers as $speaker) {
                                                 if ($speaker != "TBD") { 
