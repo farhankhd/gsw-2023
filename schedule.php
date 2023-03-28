82d35f86b2763a3428683027b30362f7d522c4de
Tiasa Kim <32970989+tiasakim@users.noreply.github.com>
2023-03-28 06:26:45 -0400

    Update schedule.php

--- schedule.php
+++ schedule.php
@@ -158,14 +158,8 @@
                                 <div class="event-right event-type-<?= $eventType ?>">
                                      
                                     <h3 class="event_heading" style="font-family: myriad-pro, sans-serif; font-style: normal; font-weight: 300px; color:  #000">
-                                        <?php
-                                            if ($eventType == "panel") { 
-                       
-                                                <a href="panels.php#<?=$eventTitle?>"><?php echo "$eventTitle";?></a>
-                                            } else {
-                                                <?php echo "$eventTitle";?>
-                                            }
-                                        ?>
+                                        <?php echo "$eventTitle";?>
+                                        
                                     </h3>
                                     
                                     <h3 class="event_heading" style="font-family: myriad-pro, sans-serif; font-style: normal; font-weight: 150px; color:  #7d4270">
