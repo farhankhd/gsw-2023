2f2f91060f7b565adfbf302fe36b3adbc3b1276d
Tiasa Kim <32970989+tiasakim@users.noreply.github.com>
2023-03-30 14:47:14 +0300

    Update index.php

--- index.php
+++ index.php
@@ -472,7 +472,7 @@
         const start = () => {
             setTimeout(function() {
                 confetti.start()
-            }, 1000); // 1000 is time that after 1 second start the confetti ( 1000 = 1 sec)
+            }, 500); // 1000 is time that after 1 second start the confetti ( 1000 = 1 sec)
         };
 
         //  for stopping the confetti 
