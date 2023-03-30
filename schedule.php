5b39704ac8ed44dd276f63873056ef1e25cee25b
Tiasa Kim <32970989+tiasakim@users.noreply.github.com>
2023-03-30 16:11:42 +0300

    Update schedule.php

--- schedule.php
+++ schedule.php
@@ -8,7 +8,6 @@
     include('markup.php');
 ?>
 
-<head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
@@ -35,7 +34,6 @@
             }
         }
     </style>
-</head>
 
     <!--PAGE BANNER-->
     <section class="startup-pitch">
